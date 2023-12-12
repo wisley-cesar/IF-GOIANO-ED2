e3146dcf6692a105f68e2f319b22c847a88ae290
Wisley César <wisleycesar32@gmail.com>
2023-12-12 19:06:08 -0300

    Estrutura De Dados 2

--- Trabalho Aplicação Prática/Arvore.py
+++ Trabalho Aplicação Prática/Arvore.py
@@ -1,7 +1,5 @@
 import pickle
-
 from NoArvore import NoArvore
-
 
 class ArvoreLivros:
     def __init__(self):
@@ -36,7 +34,7 @@ class ArvoreLivros:
                 no = no.esquerda
             else:
                 no = pilha.pop()
-                print(f"Título: {no.livro.titulo}, Autor: {no.livro.autor}, Ano: {no.livro.ano_publicacao}")
+                print(f"Título: {no.livro.titulo}, Autor: {no.livro.autor}, Ano: {no.livro.ano_publicacao}, Gênero: {no.livro.genero}")
                 no = no.direita
 
     def buscar_por_titulo(self, titulo, no=None):
@@ -53,38 +51,40 @@ class ArvoreLivros:
 
         return None
 
-    def buscar_por_autor(self, autor, no=None):
+    def buscar_por_autor(self, autor):
+        return self._buscar_por_autor(self.raiz, autor)
+
+    def _buscar_por_autor(self, no, autor):
         if not no:
-            no = self.raiz
+            return None
 
-        while no:
-            if autor == no.livro.autor:
-                return no.livro
-            elif autor < no.livro.autor:
-                no = no.esquerda
-            else:
-                no = no.direita
+        if autor == no.livro.autor:
+            return no.livro
+        elif autor < no.livro.autor:
+            return self._buscar_por_autor(no.esquerda, autor)
+        else:
+            return self._buscar_por_autor(no.direita, autor)
 
-        return None
+    def buscar_por_genero(self, genero):
+        return self._buscar_por_genero(self.raiz, genero)
 
-    def buscar_por_genero(self, genero, no=None):
+    def _buscar_por_genero(self, no, genero):
         if not no:
-            no = self.raiz
+            return None
 
-        while no:
-            if genero == no.livro.genero:
-                return no.livro
-            elif genero < no.livro.genero:
-                no = no.esquerda
-            else:
-                no = no.direita
-
-        return None
+        if genero == no.livro.genero:
+            return no.livro
+        elif genero < no.livro.genero:
+            return self._buscar_por_genero(no.esquerda, genero)
+        else:
+            return self._buscar_por_genero(no.direita, genero)
 
     def salvar_dados(self, nome_arquivo='dados_livros.pkl'):
+        lista_livros = self.obter_lista_livros()
         try:
             with open(nome_arquivo, 'wb') as arquivo:
-                pickle.dump(self.raiz, arquivo)
+                for livro in lista_livros:
+                    pickle.dump(livro, arquivo)
             print("Dados salvos com sucesso.")
         except Exception as e:
             print(f"Erro ao salvar dados: {e}")
@@ -92,12 +92,19 @@ class ArvoreLivros:
     def carregar_dados(self, nome_arquivo='dados_livros.pkl'):
         try:
             with open(nome_arquivo, 'rb') as arquivo:
-                self.raiz = pickle.load(arquivo)
+                while True:
+                    try:
+                        livro = pickle.load(arquivo)
+                        print(f"Livro carregado: {livro.titulo}, Autor: {livro.autor}, Gênero: {livro.genero}, Ano: {livro.ano_publicacao}")
+                        self.inserir(livro)
+                    except EOFError:
+                        break
             print("Dados carregados com sucesso.")
         except FileNotFoundError:
             print("Arquivo não encontrado. Criando uma nova árvore.")
         except Exception as e:
             print(f"Erro ao carregar dados: {e}")
+
     def obter_lista_livros(self, no=None):
         if not no:
             no = self.raiz
@@ -115,3 +122,34 @@ class ArvoreLivros:
                 no = no.direita
 
         return lista_livros
+
+    def remover_por_titulo(self, titulo):
+        self.raiz = self._remover_por_titulo(self.raiz, titulo)
+
+    def _remover_por_titulo(self, no, titulo):
+        if not no:
+            return None
+
+        if titulo < no.livro.titulo:
+            no.esquerda = self._remover_por_titulo(no.esquerda, titulo)
+        elif titulo > no.livro.titulo:
+            no.direita = self._remover_por_titulo(no.direita, titulo)
+        else:
+            # Nó a ser removido encontrado
+            if not no.esquerda:
+                return no.direita
+            elif not no.direita:
+                return no.esquerda
+
+            # Nó tem dois filhos, encontrar sucessor (menor valor na subárvore direita)
+            sucessor = self._min_valor_no(no.direita)
+            no.livro = sucessor.livro
+            no.direita = self._remover_por_titulo(no.direita, sucessor.livro.titulo)
+
+        return no
+
+    def _min_valor_no(self, no):
+        atual = no
+        while atual.esquerda:
+            atual = atual.esquerda
+        return atual
--- Trabalho Aplicação Prática/CLIInterface.py
+++ Trabalho Aplicação Prática/CLIInterface.py
@@ -1,5 +1,3 @@
-# Em CLIInterface.py, certifique-se de incluir todos os métodos necessários
-
 from Arvore import ArvoreLivros
 from Livro import Livro
 from OrdenadorLivros import OrdenadorLivros
@@ -7,6 +5,7 @@ from OrdenadorLivros import OrdenadorLivros
 class CLIInterface:
     def __init__(self):
         self.arvore_livros = ArvoreLivros()
+        self.arvore_livros.carregar_dados()  # Carregar dados ao iniciar o programa
 
     def menu(self):
         while True:
@@ -17,8 +16,9 @@ class CLIInterface:
             print("5. Buscar por título")
             print("6. Buscar por autor")
             print("7. Buscar por gênero")
-            print("8. Salvar dados")
-            print("9. Sair")
+            print("8. Remover livro por título")
+            print("9. Salvar dados")
+            print("10. Sair")
 
             choice = input("Escolha uma opção: ")
 
@@ -37,18 +37,26 @@ class CLIInterface:
             elif choice == "7":
                 self.buscar_por_genero()
             elif choice == "8":
-                self.arvore_livros.salvar_dados()
+                self.remover_livro_por_titulo()
             elif choice == "9":
+                self.arvore_livros.salvar_dados()
+            elif choice == "10":
                 print("Saindo do programa. Até mais!")
                 self.arvore_livros.salvar_dados()
                 break
             else:
                 print("Opção inválida. Tente novamente.")
+
+    def remover_livro_por_titulo(self):
+        titulo_a_remover = input("Digite o título do livro a ser removido: ").strip()
+        self.arvore_livros.remover_por_titulo(titulo_a_remover)
+        print("Livro removido com sucesso.")
+
     def cadastrar_livro(self):
-        titulo = input("Digite o título do livro: ")
-        autor = input("Digite o autor do livro: ")
-        genero = input("Digite o gênero do livro: ")
-        ano_publicacao = input("Digite o ano de publicação do livro: ")
+        titulo = input("Digite o título do livro: ").strip()
+        autor = input("Digite o autor do livro: ").strip()
+        genero = input("Digite o gênero do livro: ").strip()
+        ano_publicacao = input("Digite o ano de publicação do livro: ").strip()
 
         livro = Livro(titulo, autor, genero, int(ano_publicacao))
         self.arvore_livros.inserir(livro)
@@ -63,36 +71,35 @@ class CLIInterface:
         livros_ordenados_por_titulo = OrdenadorLivros.quicksort_por_titulo(livros)
         print("\nLivros ordenados por título:")
         for livro in livros_ordenados_por_titulo:
-            print(f"Título: {livro.titulo}, Autor: {livro.autor}, Ano: {livro.ano_publicacao}")
+            print(f"Título: {livro.titulo}, Autor: {livro.autor}, Ano: {livro.ano_publicacao}, Gênero: {livro.genero}")
 
     def ordenar_livros_por_genero(self):
         livros = self.arvore_livros.obter_lista_livros()
         livros_ordenados_por_genero = OrdenadorLivros.quicksort_por_genero(livros)
         print("\nLivros ordenados por gênero:")
         for livro in livros_ordenados_por_genero:
-            print(f"Título: {livro.titulo}, Autor: {livro.autor}, Ano: {livro.ano_publicacao}")
+            print(f"Título: {livro.titulo}, Autor: {livro.autor}, Ano: {livro.ano_publicacao}, Gênero: {livro.genero}")
 
     def buscar_por_titulo(self):
-        titulo_a_buscar = input("Digite o título a ser buscado: ")
+        titulo_a_buscar = input("Digite o título a ser buscado: ").strip()
         livro_encontrado = self.arvore_livros.buscar_por_titulo(titulo_a_buscar)
         if livro_encontrado:
-            print(f"\nLivro encontrado por título: {livro_encontrado.titulo}, Autor: {livro_encontrado.autor}")
+            print(f"\nLivro encontrado por título: {livro_encontrado.titulo}, Autor: {livro_encontrado.autor}, Gênero: {livro_encontrado.genero}")
         else:
             print("Livro não encontrado.")
 
     def buscar_por_autor(self):
-        autor_a_buscar = input("Digite o autor a ser buscado: ")
+        autor_a_buscar = input("Digite o autor a ser buscado: ").strip()
         livro_encontrado = self.arvore_livros.buscar_por_autor(autor_a_buscar)
         if livro_encontrado:
-            print(f"\nLivro encontrado por autor: {livro_encontrado.titulo}, Autor: {livro_encontrado.autor}")
+            print(f"\nLivro encontrado por autor: {livro_encontrado.titulo}, Autor: {livro_encontrado.autor}, Gênero: {livro_encontrado.genero}")
         else:
             print("Livro não encontrado.")
 
     def buscar_por_genero(self):
-        genero_a_buscar = input("Digite o gênero a ser buscado: ")
+        genero_a_buscar = input("Digite o gênero a ser buscado: ").strip()
         livro_encontrado = self.arvore_livros.buscar_por_genero(genero_a_buscar)
         if livro_encontrado:
-            print(f"\nLivro encontrado por gênero: {livro_encontrado.titulo}, Autor: {livro_encontrado.autor}")
+            print(f"\nLivro encontrado por gênero: {livro_encontrado.titulo}, Autor: {livro_encontrado.autor}, Gênero: {livro_encontrado.genero}")
         else:
             print("Livro não encontrado.")
-
